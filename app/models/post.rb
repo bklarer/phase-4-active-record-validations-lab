7fafd21d06c5c2c87afd0e3cf708d5e3c3cbbb7b
Benjamin Klarer <benjaminklarer@gmail.com>
2022-11-18 11:41:50 -0700

    commit tree d9fd0d1cdf87ba1fb787a2e9bd85a5d2a917b235

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,27 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250} 
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :clickbait?
+
+    @@clickbait_patterns = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+
+    
+    def clickbait?
+        if @@clickbait_patterns.none? {|pattern| pattern.match title}
+            errors.add(:title, "must be clickbait")
+        end
+
+    end
+
+
+
 end
+
